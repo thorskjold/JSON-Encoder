4e606cecd046c366b26b586036b41961f2e5b676
Thor <thorlindberg@icloud.com>
2021-01-29 03:43:50 +0100

    Initial Commit

---
+++ Shared/JSON_EncoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSON_EncoderApp.swift
+//  Shared
+//
+//  Created by Thor Lindberg on 29/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct JSON_EncoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
